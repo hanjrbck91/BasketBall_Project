878f726b60413d38b2be843ca266d41052408487
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2023-11-13 20:47:22 +0530

    last push

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,18 +8,20 @@ public class Player : MonoBehaviour
     public GameObject playerCamera;
 
     public float ballDistance = 2f;
-    public float maxThrowingForce = 1000f; // Adjust this value for the maximum throwing force
+    public float ballThrowingforce = 550f;
     public Rigidbody rb;
 
     public bool holdingBall = true;
-    private float throwStartTime;
 
+    // Start is called before the first frame update
     void Start()
     {
+
         rb = ball.GetComponent<Rigidbody>();
         rb.useGravity = false;
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (holdingBall)
@@ -26,33 +30,10 @@ public class Player : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-                throwStartTime = Time.time;
-            }
-
-            if (Input.GetMouseButton(0))
-            {
-                // Calculate throwing force based on the duration the mouse button is held down
-                float throwDuration = Time.time - throwStartTime;
-                float normalizedThrowForce = Mathf.Clamp01(throwDuration / maxThrowingForce);
-                float throwingForce = normalizedThrowForce * maxThrowingForce;
-
-                // Optional: Visualize the throwing force (you can remove this line if not needed)
-                Debug.Log("Throwing Force: " + throwingForce);
-
-                // Update the ball's position based on the mouse movement (optional)
-                ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * (ballDistance + throwDuration);
-
-                // Note: You may want to add additional logic to limit the maximum throw distance
-
-            }
-
-            if (Input.GetMouseButtonUp(0))
-            {
                 holdingBall = false;
+                //ball.ActivateTrail();
                 rb.useGravity = true;
-
-                // Apply the final throwing force
-                rb.AddForce(playerCamera.transform.forward * maxThrowingForce);
+                rb.AddForce(playerCamera.transform.forward * ballThrowingforce);
             }
         }
     }
